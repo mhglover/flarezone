63981bd305e945f507504e00d4641bbd873490a8
Matthew Harris Glover <mglover@carnegielearning.com>
2018-01-30 16:03:50 -0500

    better formatting

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,12 @@
-import sys, os
-cwd = os.getcwd()
-INTERP = cwd + "/env/bin/python"
-
-#INTERP is present twice so that the new python interpreter knows the actual executable path 
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-
+#!/usr/bin/env python
+""" Set up Passenger for a virtualenv and the flarezone app. """
+# pylint: disable=W0611
+import sys
+import os
 from flarezone import app as application
+
+INTERP = os.getcwd() + "/env/bin/python"
+#INTERP is present twice so that the new python interpreter
+# knows the actual executable path
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
